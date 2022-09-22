b7025e6c037bf47f5f6b3e1bb4fb41ddfba04e69
Rebecca Johnson <rebeccajohnson@commure.com>
2022-09-22 17:37:29 -0400

    Week 3 Submit

---
+++ Week3/Question4.java
@@ -0,0 +1,25 @@
+package Week3;
+
+public class Question4 {
+/* Store 20 in the speed variable.
+Store 10 in the time variable.
+Multiply speed by time and store the result in the distance variable.
+Display the contents of the distance variable.*/
+
+    public static void main(String[] args)
+    {
+        int speed = 20;
+        int time = 10;
+        int distance = speed * time;
+
+        System.out.println("Distance: "+ distance);
+
+        System.out.print("This is line 1.\n\n\n");
+        System.out.print("This is line 2.\n\n");
+        System.out.print("This is line 3.\n\n");
+        System.out.print("This is line 4.\n\n");
+        System.out.println("This is line 5.");
+
+    }
+
+}
