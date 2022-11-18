1576329bb63c15bc869fdd80e17ee53c910f1364
rafjohnson <rafavreau@gmail.com>
2022-11-17 22:24:39 -0500

    Setup classes

---
+++ Week11/ParkingTicket/ParkedCar.java
@@ -0,0 +1,57 @@
+package Week11.ParkingTicket;
+
+import javax.swing.text.PlainDocument;
+
+public class ParkedCar {
+ /*
+ Parked car keeps track of make, model, color
+ License number, and number of minutes parked.
+
+ Getters: yes
+ Setters: yes
+ Methods: none?
+  */   
+
+  private String make;
+  private String model;
+  private String color;
+  private String licenseNumber;
+  private Integer minutesParked;
+
+  //constructor - car info only, set minutes parked separately since that would likely update in real time
+  public ParkedCar(String pMake, String pModel, String pColor, String pLicenseNumber )
+  {
+    make = pMake;
+    model = pModel;
+    color = pColor;
+    licenseNumber = pLicenseNumber; 
+
+  }
+    
+  //getters
+  public String getMake()
+  {
+    return make;
+  }
+
+  public String getModel()
+  {
+    return model;
+  }
+
+  public String getColor()
+  {
+    return color;
+  }
+
+  public String getLicenseNumber()
+  {
+    return licenseNumber;
+  }
+
+  public int getMinutesParked()
+  {
+    return minutesParked;
+  }
+
+}
